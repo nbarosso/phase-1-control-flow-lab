9e57507a10457d2f491dc86421d95223040b6753
Nicholas Barosso <nbarosso@gmail.com>
2023-08-12 10:25:36 -0500

    comepleted lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numberInFeet){
+  if(numberInFeet <= 400) {
+    return 'This one is on me!'
+  }
+  else if(numberInFeet > 400 && numberInFeet <= 2000) {
+    return `That will be twenty bucks.`
+  }
+  else if(numberInFeet > 2000 && numberInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(numberInFeet > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC'?"Ok, sounds good." : "No go."
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipInDollars){
+  switch(tipInDollars) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
